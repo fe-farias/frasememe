bb2b622e13c62dda3aef183fb3b56ab68faf97aa
Felipe Farias <fefarias97@gmail.com>
2021-09-02 16:05:47 -0300

    Resolução exercício frasememe

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace frasememe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frasedigitada, frasememetonervoso;
+            Console.WriteLine("O que você acha de mim? ");
+            frasedigitada = Console.ReadLine();
+
+            frasememetonervoso = $"{frasedigitada.Remove(10)}... NÃO NÃO, PERA. TÔ NERVOSO";
+
+            Console.WriteLine(frasememetonervoso);
+
+        }
+    }
+}
